e9bdf8bcf7d3cecf3533ba75823587c7ffd14cc7
FlyingBird94 <102919352+FlyGit94@users.noreply.github.com>
2026-09-21 10:00:15 +0800

    Update tailwind.config.js

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -10,6 +10,8 @@ export default {
       colors: {
         dark: '#0d0d0d',
         card: '#121212',
+        cream: 'rgb(233, 221, 200)',    // 书写阅读底色
+        ink: '#1a1a1a',                  // 字体加深
       }
     },
   },
